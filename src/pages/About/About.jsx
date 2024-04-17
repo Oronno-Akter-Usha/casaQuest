2c9f3083a1b407b90171c578615df2f1c83f289f
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-04-17 22:33:22 +0600

    add content in about and contact us page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,12 +2,32 @@ import { Helmet } from "react-helmet-async";
 
 const About = () => {
   return (
-    <div>
+    <div className="mt-20 mx-5">
       <Helmet>
         <title>Casa Quest-About page</title>
         <link rel="" href="/" />
       </Helmet>
-      <h2>About</h2>
+      <h2 className="text-2xl md:text-3xl font-bold">About Us</h2>
+      <p className="mt-3 md:mt-6">
+        At CasaQuest, we believe that finding the perfect home is not just about
+        square footage and amenities; it is about discovering a place where
+        memories are made, dreams are realized, and lives are enriched. With a
+        passion for real estate and a commitment to exceptional service,
+        CasaQuest is your dedicated partner on the journey to finding your ideal
+        residence. Founded with the vision of providing personalized guidance
+        and expert advice, CasaQuest brings together a team of experienced
+        professionals who are deeply invested in the well-being and satisfaction
+        of our clients. Whether you are a first-time buyer, seasoned investor,
+        or relocating to a new city, we are here to support you every step of
+        the way. Our approach is built on integrity, transparency, and a genuine
+        desire to exceed expectations. We take the time to understand your
+        unique preferences, priorities, and goals, ensuring that each property
+        we present aligns perfectly with your vision of home. At CasaQuest, we
+        do not just help you find a place to live; we help you find your
+        sanctuary, your haven, your Casa. Let us be your guide as you embark on
+        this exciting journey of discovery and fulfillment. Welcome to
+        CasaQuest, where your dream home becomes a reality.
+      </p>
     </div>
   );
 };
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,12 +2,62 @@ import { Helmet } from "react-helmet-async";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="mt-20 mx-5">
       <Helmet>
         <title>Casa Quest-Contact page</title>
         <link rel="" href="/" />
       </Helmet>
-      <h2>Contact</h2>
+      {/* <h2 className="text-2xl md:text-3xl font-bold">Contact Us</h2> */}
+
+      <div className="hero min-h-screen ">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold text-center">Contact Us</h1>
+            <p className="py-6 text-center">contact us today!</p>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div>
+                <label className="label">
+                  <span className="label-text">Message</span>
+                </label>
+                <textarea
+                  name=""
+                  id=""
+                  className="border p-4 rounded w-full max-w-xs"
+                  placeholder="Message"
+                ></textarea>
+              </div>
+
+              <div className="form-control mt-6">
+                <button className="btn bg-[#5d0af7] text-white">Send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
