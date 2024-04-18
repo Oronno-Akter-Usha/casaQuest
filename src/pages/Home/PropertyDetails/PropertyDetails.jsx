23cb8eb431ba604ae940de00e94098f5a91732c8
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-04-18 19:37:41 +0600

    added aos

--- src/pages/Home/PropertyDetails/PropertyDetails.jsx
+++ src/pages/Home/PropertyDetails/PropertyDetails.jsx
@@ -39,23 +39,39 @@ const PropertyDetails = () => {
       <div className="md:col-span-2">
         <div className="flex justify-between mb-5">
           <div>
-            <h2 className="font-bold md:text-3xl">{estate_title}</h2>
-            <p className="flex items-center gap-1 md:text-xl mt-2">
+            <h2
+              className="font-bold md:text-3xl"
+              data-aos="fade-right"
+              data-aos-duration="1000"
+            >
+              {estate_title}
+            </h2>
+            <p
+              className="flex items-center gap-1 md:text-xl mt-2"
+              data-aos="fade-right"
+              data-aos-duration="1000"
+            >
               <IoLocationOutline className="text-lg font-medium" />
               {location}
             </p>
           </div>
-          <p className="font-bold md:text-3xl">{price}</p>
+          <p
+            className="font-bold md:text-3xl"
+            data-aos="fade-left"
+            data-aos-duration="1000"
+          >
+            {price}
+          </p>
         </div>
         <div className="flex justify-between items-center">
-          <div>
+          <div data-aos="fade-right" data-aos-duration="1000">
             <span className="px-3 py-1 bg-[#242425d6] text-white rounded text-sm mb-2">
               <span className="mr-1">For</span>
               {status}
             </span>
           </div>
 
-          <Link to={""}>
+          <Link to={""} data-aos="fade-left" data-aos-duration="2000">
             <button
               onClick={handleWishlist}
               className="btn px-5 bg-[#7a42ff] text-white hover:text-[#7a42ff] mb-5"
@@ -64,10 +80,20 @@ const PropertyDetails = () => {
             </button>
           </Link>
         </div>
-        <img className="w-full md:h-auto rounded" src={image} alt="" />
+        <img
+          className="w-full md:h-auto rounded"
+          src={image}
+          alt=""
+          data-aos="fade-up"
+          data-aos-duration="2000"
+        />
 
         {/* overview */}
-        <div className="shadow-md bg-[#eae5ff35] p-5 mt-8 mb-4 rounded">
+        <div
+          className="shadow-md bg-[#eae5ff35] p-5 mt-8 mb-4 rounded"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
           <p className="font-bold text-lg md:text-xl mt-8 mb-4">Overview</p>
           <hr />
 
@@ -99,14 +125,22 @@ const PropertyDetails = () => {
         </div>
 
         {/* description */}
-        <div className="shadow-md bg-[#eae5ff35] p-8 mt-8 mb-4 rounded">
+        <div
+          className="shadow-md bg-[#eae5ff35] p-8 mt-8 mb-4 rounded"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
           <p className="mt-8 mb-4 font-bold text-lg md:text-xl">Description</p>
           <hr />
           <p className="mt-4">{description}</p>
         </div>
 
         {/* features */}
-        <div className="shadow-md bg-[#eae5ff35] p-8 mt-8 mb-4 rounded">
+        <div
+          className="shadow-md bg-[#eae5ff35] p-8 mt-8 mb-4 rounded"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
           <p className="mt-8 mb-4 font-bold text-lg md:text-xl">Features</p>
           <hr />
           <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-between mt-4">
@@ -125,7 +159,12 @@ const PropertyDetails = () => {
 
       {/* schedule  */}
 
-      <div className="shadow-md bg-[#eae5ff35] p-8 mt-8 mb-4 rounded h-auto text-center">
+      <div
+        className="shadow-md bg-[#eae5ff35] p-8 mt-8 mb-4 rounded h-auto text-center"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        data-aos-delay="1000"
+      >
         <h2 className="text-2xl font-bold mb-5">Schedule a Tour</h2>
         <form className="space-y-4">
           <input
